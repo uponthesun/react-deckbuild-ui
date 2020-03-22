bb127a06cfd8f2bb7a22b3a1a1800ff76a9e76f7
Winter Dong <winter@code.org>
2020-03-22 00:29:21 -0700

    Extract Card component

--- src/index.js
+++ src/index.js
@@ -5,23 +5,29 @@ import './index.css';
 const cards = [['Battle Hymn', 'Reaper King', 'Death or Glory'],
                 ['Mindless Automaton', 'Wizard Mentor']];
 
+class Card extends React.Component {
+  render() {
+    const imageURL = `https://api.scryfall.com/cards/named?format=image&exact=${encodeURI(this.props.cardName)}`;
+    return (
+      <img
+        src={imageURL} width="146" height="204"
+        style={{
+          position: "absolute",
+          top: `${40 * this.props.stackIndex}px`,
+          left: `${147 * this.props.col}px`,
+          zIndex: `${this.props.stackIndex}`
+        }} />
+    );
+  }
+}
+
 class Space extends React.Component {
   renderCards(cards) {
     const cardImages = [];
 
     for (var col = 0; col < cards.length; col++) {
       for (var i = 0; i < cards[col].length; i++) {
-        const card = cards[col][i];
-        const imageURL = `https://api.scryfall.com/cards/named?format=image&exact=${encodeURI(card)}`;
-        const image = <img
-                        src={imageURL} width="146" height="204"
-                        style={{
-                          position: "absolute",
-                          top: `${40 * i}px`,
-                          left: `${147 * col}px`,
-                          zIndex: `${i}`
-                        }} />
-        cardImages.push(image);
+        cardImages.push(<Card cardName={cards[col][i]} stackIndex={i} col={col} />);
       }
     }
 
