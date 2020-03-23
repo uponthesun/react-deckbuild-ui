adbc85e502dcd9405282fac18c1decf2f6f9580c
Winter Dong <winter@code.org>
2020-03-22 17:04:39 -0700

    Cleanup: - extract board state to BoardState - card positioning to Board component - renames + comments

--- src/index.js
+++ src/index.js
@@ -10,68 +10,86 @@ const IMG_HEIGHT = 204;
 const COL_WIDTH = IMG_WIDTH + 1;
 const CARD_STACKING_OFFSET = 40;
 const NUM_COLS = 8;
-const SPACE_COLUMNS = [...Array(NUM_COLS)].map(_ => []);
-const INITIAL_CARD_NAMES = [['Battle Hymn', 'Reaper King', 'Death or Glory'],
-                            ['Mindless Automaton', 'Wizard Mentor']];
-var nextId = 0;
-for (var i = 0; i < INITIAL_CARD_NAMES.length; i++) {
-  for (var name of INITIAL_CARD_NAMES[i]) {
-    SPACE_COLUMNS[i].push({name: name, cardId: nextId});
-    nextId++;
+const INITIAL_CARD_NAMES = ['Battle Hymn', 'Reaper King', 'Death or Glory', 'Mindless Automaton', 'Wizard Mentor'];
+
+class BoardState {
+  constructor(initialCards) {
+    this.cardColumns = [...Array(NUM_COLS)].map(_ => []); // Initialize with NUM_COLS empty arrays
+    this.nextId = 0;
+
+    if (initialCards) {
+      initialCards.forEach(c => this.addCard(c));
+    }
+  }
+
+  addCard(cardName) {
+    this.cardColumns[0].push({name: cardName, id: this.nextId});
+    this.nextId++; // TODO: Do we need to worry about concurrency? I don't think so but not positive
+  }
+
+  moveCard(id, newCol, newIndexInCol) {
+    // remove card from current position
+    // TODO: could consider storing mapping of id to position to avoid for loop lookup
+    var cardToMove;
+    for (var col = 0; col < this.cardColumns.length; col++) {
+      const cardCol = this.cardColumns[col];
+      for (var i = 0; i < cardCol.length; i++) {
+        if (cardCol[i].id === id) {
+          cardToMove = cardCol[i];
+          cardCol.splice(i, 1);
+        }
+      }
+    }
+
+    // add card to new position
+    newIndexInCol = Math.min(newIndexInCol, this.cardColumns[newCol].length);
+    this.cardColumns[newCol].splice(newIndexInCol, 0, cardToMove);
   }
 }
-console.log(JSON.stringify(SPACE_COLUMNS));
 
+// Figure out how to maintain this state inside Board component cleanly?
+//DND hooks force Board to be a function component
+const BOARD_STATE = new BoardState(INITIAL_CARD_NAMES);
+
+// Card react component - displays a single draggable card
 function Card(props) {
   const [_, drag] = useDrag({
-    item: { type: "Card", name: props.name, cardId: props.cardId },
+    item: { type: "Card", card: props.card },
   });
 
-  const imageURL = `https://api.scryfall.com/cards/named?format=image&exact=${encodeURI(props.name)}`;
+  const imageURL = `https://api.scryfall.com/cards/named?format=image&exact=${encodeURI(props.card.name)}`;
   return (
     <img
       ref={drag}
       src={imageURL} width={IMG_WIDTH} height={IMG_HEIGHT}
       style={{
         position: "absolute",
-        top: `${CARD_STACKING_OFFSET * props.stackIndex}px`,
-        left: `${COL_WIDTH * props.col}px`,
-        zIndex: `${props.stackIndex}`
+        top: `${props.top}px`,
+        left: `${props.left}px`,
+        zIndex: `${props.zIndex}`,
       }} />
   );
 }
 
-function dropCard(clientOffset, card) {
-  for (var col = 0; col < SPACE_COLUMNS.length; col++) {
-    for (var i = 0; i < SPACE_COLUMNS[col].length; i++) {
-      if (SPACE_COLUMNS[col][i].cardId === card.cardId) {
-        SPACE_COLUMNS[col].splice(i, 1);
-      }
-    }
-  }
-
-  const newCol = Math.floor(clientOffset.x / COL_WIDTH);
-  const newStackIndex = Math.min(Math.floor(clientOffset.y / CARD_STACKING_OFFSET), SPACE_COLUMNS[newCol].length);
-
-  SPACE_COLUMNS[newCol].splice(newStackIndex, 0, {name: card.name, cardId: card.cardId});
-  console.log(JSON.stringify(SPACE_COLUMNS));
-}
-
-function Space(props) {
+// Board react component - the space that all the cards are in
+function Board(props) {
   const [{ mousePos }, drop] = useDrop({
     accept: "Card",
-    drop: (item, monitor) => dropCard(monitor.getClientOffset(), item),
+    drop: (item, monitor) => dropCard(monitor.getClientOffset(), item.card),
     collect: monitor => ({
       mousePos: monitor.getClientOffset(),
     }),
   })
 
   const cardImages = [];
+  const cardColumns = BOARD_STATE.cardColumns;
+  for (var col = 0; col < cardColumns.length; col++) {
+    for (var i = 0; i < cardColumns[col].length; i++) {
+      const card = cardColumns[col][i]
+      const topOffset = CARD_STACKING_OFFSET * i;
+      const leftOffset = COL_WIDTH * col;
 
-  for (var col = 0; col < SPACE_COLUMNS.length; col++) {
-    for (var i = 0; i < SPACE_COLUMNS[col].length; i++) {
-      const cardObj = SPACE_COLUMNS[col][i]
-      cardImages.push(<Card name={cardObj.name} cardId={cardObj.cardId} key={cardObj.cardId.toString()} col={col} stackIndex={i} />);
+      cardImages.push(<Card key={card.id.toString()} card={card} top={topOffset} left={leftOffset} zIndex={i} />);
     }
   }
 
@@ -82,7 +100,14 @@ function Space(props) {
   );
 }
 
+function dropCard(clientOffset, card) {
+  const newCol = Math.floor(clientOffset.x / COL_WIDTH);
+  const newIndexInCol = Math.floor(clientOffset.y / CARD_STACKING_OFFSET);
+
+  BOARD_STATE.moveCard(card.id, newCol, newIndexInCol);
+}
+
 ReactDOM.render(
-  <DndProvider backend={Backend}> <Space /> </DndProvider>,
+  <DndProvider backend={Backend}> <Board /> </DndProvider>,
   document.getElementById('root')
 );
