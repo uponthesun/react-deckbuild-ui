e91ab9496c64b7523a3843e2f8b360d419797dc4
Winter Dong <winter@code.org>
2021-01-01 01:34:31 -0800

    Updates

--- precache-manifest.a0aba1dc85b85a06edbb8164961418af.js
+++ precache-manifest.a0aba1dc85b85a06edbb8164961418af.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c38efb2befd735f3031ae5e9e965e5b9",
+    "revision": "49b3775897f529893f1b77099af582ce",
     "url": "/react-deckbuild-ui/index.html"
   },
   {
-    "revision": "28511f0be2fcd822a617",
+    "revision": "49ae69198b447cb96dfd",
     "url": "/react-deckbuild-ui/static/css/main.9ea0f271.chunk.css"
   },
   {
-    "revision": "0c385a1096fb8aadd93c",
-    "url": "/react-deckbuild-ui/static/js/2.3dfede7c.chunk.js"
+    "revision": "6b9df0fdc579b1fcdde2",
+    "url": "/react-deckbuild-ui/static/js/2.b6d99e9a.chunk.js"
   },
   {
     "revision": "531d7b2065717e21d8dbdd1394bf24a4",
-    "url": "/react-deckbuild-ui/static/js/2.3dfede7c.chunk.js.LICENSE.txt"
+    "url": "/react-deckbuild-ui/static/js/2.b6d99e9a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "28511f0be2fcd822a617",
-    "url": "/react-deckbuild-ui/static/js/main.d3679640.chunk.js"
+    "revision": "49ae69198b447cb96dfd",
+    "url": "/react-deckbuild-ui/static/js/main.e5502587.chunk.js"
   },
   {
     "revision": "2c2c8309474af146de74",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deckbuild-ui/precache-manifest.4719997fe246364f2a4c1d11be30b67a.js"
+  "/react-deckbuild-ui/precache-manifest.a0aba1dc85b85a06edbb8164961418af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deckbuild-ui/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
